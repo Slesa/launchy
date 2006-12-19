53b5b1c56e2db9aa5e51654483024079929a76ed
karlinjf <karlinjf@a198a55a-610f-0410-8134-935da841aa8c>
2006-12-19 20:16:02 +0000

    Plugins starting to come along.  Just wrote a parser for various actions in the search box.

--- Launchy_VC7/Launchy.h
+++ Launchy_VC7/Launchy.h
@@ -28,7 +28,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "resource.h"		// main symbols
 
-
+extern CStringArray SearchStrings;
+extern int SearchPluginID;
 extern CString searchTxt;
 
 // CLaunchyApp:
