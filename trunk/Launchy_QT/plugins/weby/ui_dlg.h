05b92e3c6ee834b73ccb93ddb7b640ced9e43333
karlinjf <karlinjf@a198a55a-610f-0410-8134-935da841aa8c>
2008-01-15 02:39:33 +0000

    Undid the change that I'd made to catalog.h  which was causing trouble for plugin makers.

--- trunk/Launchy_QT/plugins/weby/ui_dlg.h
+++ trunk/Launchy_QT/plugins/weby/ui_dlg.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'dlg.ui'
 **
-** Created: Tue Dec 18 13:43:05 2007
+** Created: Mon Jan 14 19:30:53 2008
 **      by: Qt User Interface Compiler version 4.3.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
