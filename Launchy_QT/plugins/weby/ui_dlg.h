8acdcf991c71cb81fd760c864d06c1e0a932fb7c
karlinjf <karlinjf@a198a55a-610f-0410-8134-935da841aa8c>
2008-07-09 20:51:27 +0000

    small change, no parent

--- Launchy_QT/plugins/weby/ui_dlg.h
+++ Launchy_QT/plugins/weby/ui_dlg.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'dlg.ui'
 **
-** Created: Sun Jul 6 21:14:33 2008
+** Created: Wed Jul 9 13:53:45 2008
 **      by: Qt User Interface Compiler version 4.4.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
