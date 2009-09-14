069e2326dbdb0e8323aa55c970ac19bdd7d46589
simonca <simonca@a198a55a-610f-0410-8134-935da841aa8c>
2009-09-14 07:58:04 +0000

    Uploaded missing file

---
+++ Launchy_QT/platforms/win/WinIconProvider.h
@@ -0,0 +1,32 @@
+#ifndef __WINICONPROVIDER_H
+#define __WINICONPROVIDER_H
+
+
+#include <windows.h>
+#include <shlobj.h>
+#include <QFileIconProvider>
+
+
+class WinIconProvider : QFileIconProvider
+{
+public:
+	WinIconProvider();
+	~WinIconProvider();
+
+	virtual QIcon icon(const QFileInfo& info) const;
+
+private:
+	HIMAGELIST GetSystemImageListHandle( bool bSmallIcon );
+	int GetFileIconIndex( QString strFileName , BOOL bSmallIcon ) const;
+	int GetDirIconIndex(BOOL bSmallIcon );
+	HICON GetFileIconHandle(QString strFileName, BOOL bSmallIcon);
+	HICON GetIconHandleNoOverlay(QString strFileName, BOOL bSmallIcon) const;
+	HICON GetFolderIconHandle(BOOL bSmallIcon );
+	QString GetFileType(QString strFileName);
+	QPixmap convertHIconToPixmap( const HICON icon) const;
+
+	HIMAGELIST iconlist;	
+};
+
+
+#endif
