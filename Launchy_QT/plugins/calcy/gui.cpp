afad30a1f374708b8ff522a5c32e3e05b448a038
simonca <simonca@a198a55a-610f-0410-8134-935da841aa8c>
2010-03-17 16:41:17 +0000

    Added copy to clipboard option

--- Launchy_QT/plugins/calcy/gui.cpp
+++ Launchy_QT/plugins/calcy/gui.cpp
@@ -32,6 +32,7 @@ Gui::Gui(QWidget* parent)
 
 	txtRounding->setValue(settings->value("calcy/outputRounding", 10).toInt());
 	chkDigitGrouping->setChecked(settings->value("calcy/outputGroupSeparator", true).toBool());
+	chkCopyToClipboard->setChecked(settings->value("calcy/copyToClipboard", true).toBool());
 }
 
 
@@ -49,4 +50,5 @@ void Gui::writeOptions()
 
 	settings->setValue("calcy/outputRounding", txtRounding->value());
 	settings->setValue("calcy/outputGroupSeparator", chkDigitGrouping->isChecked());
+	settings->setValue("calcy/copyToClipboard", chkCopyToClipboard->isChecked());
 }
