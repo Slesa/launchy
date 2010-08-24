81fc5db94ef745f96f5580018bad2598be12c488
karlinjf <karlinjf@a198a55a-610f-0410-8134-935da841aa8c>
2010-08-24 23:45:30 +0000

    Fixed calcy's regex to support numbers starting with '.'

--- Launchy_QT/src/globals.h
+++ Launchy_QT/src/globals.h
@@ -23,8 +23,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "SettingsManager.h"
 #include <QSettings> // need this for os x
-#define LAUNCHY_VERSION 251
-#define LAUNCHY_VERSION_STRING "2.5.1 Beta 2"
+#define LAUNCHY_VERSION 260
+#define LAUNCHY_VERSION_STRING "2.6 Beta 1"
 
 #define HASH_LAUNCHY 0
 #define HASH_HISTORY 1
