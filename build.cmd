458d1798edf91eb1f75129abd20494d8efdbddee
Jörg Preiß <joerg.preiss@slesa.de>
2016-06-30 00:01:41 +0200

    Adding cmake files for libraries

--- build.cmd
+++ build.cmd
@@ -1,24 +1,77 @@
 @echo off
 
-PATH=%QTDIR%\bin;%PATH%
 call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+SET LAUNCHY_VERSION=3.0.0
+SET PPATH=%PATH%
 
-echo Creating NSIS script...
+
+:BUILD
+
+echo "Creating 64 bit..."
+SET QTDIR="%QTPATH%\msvc2013_64"
+SET PATH=%QTDIR%\bin;%PPATH%
+
+CALL :MakeAll bin\release win32-msvc2013
+if errorlevel 1 (
+  goto MENU
+) 
+
+REM echo "Creating 32 bit..."
+REM call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+REM SET QTDIR="%QTPATH%\msvc2013"
+REM SET PATH=%QTDIR%\bin;%PPATH%
+
+REM CALL :MakeAll bin\release.32 win32-msvc2013
+
+SET PATH=%PPATH%
+
+rem Loop the build script.
+:MENU
+set CHOICE=nothing
+echo (Q)uit, (Enter) runs the build again
+set /P CHOICE= 
+if /i "%CHOICE%"=="Q" goto :Quit
+
+GOTO Build
+
+:Quit
+exit /b %errorlevel% 
+
+
+
+:MakeAll
+
+echo Cleaning %1 ...
+if EXIST %1 rmdir /s /q %1
+
+echo Creating Makefiles ...
 cd src
-%QTDIR%\bin\qmake -recursive Launchy.pro "CONFIG+=release" 
+%QTDIR%\bin\qmake -recursive -spec %2 Launchy.pro "CONFIG+=release" 
+if errorlevel 1 (
+  cd ..
+  exit /b %errorlevel%
+) 
 
-rem echo Building...
-rem nmake
+echo Building...
+nmake
+if errorlevel 1 (
+  cd ..
+  exit /b %errorlevel%
+) 
 cd ..
 
 echo Deploying Qt...
 %QTDIR%\bin\windeployqt --release --no-plugins --no-webkit bin\release\app
+if errorlevel 1 (
+  exit /b %errorlevel%
+) 
 
 echo Moving translation files
 move bin\release\app\*.qm bin\release\app\tr
+if errorlevel 1 (
+  exit /b %errorlevel%
+) 
 
 echo Creating setup file...
 tools\nsis\bin\makensis setup\win\setup.nsi
-echo Done
-pause
 
--- src/common/common.pro
+++ src/common/common.pro
@@ -5,9 +5,9 @@ TARGET			= launchy.common
 CONFIG			+= staticlib
 win:CONFIG		+= qpa
 QT				+= widgets
-win32:QT		+= winextras gui-private
-linux:QT    	+= x11extras
-mac:QT			+= macextras
+win32:QT        += winextras gui-private
+linux:QT        += x11extras
+macx:QT         += macextras
 
 HEADERS 		= \
                 catalog.h \
@@ -27,27 +27,27 @@ win32:HEADERS	+= \
                 win_platform.h \
                 win_minidump.h \
                 win_util.h
-mac:HEADERS		+= \
+macx:HEADERS    += \
                 mac_platform.h \
                 mac_keytrigger.h
 
-SOURCES 		= \
+SOURCES         = \
                 catalog.cpp \
                 globals.cpp \
                 platform_base.cpp \
                 globalshortcutmanager.cpp \
                 settingsmanager.cpp
-linux:SOURCES	+= x11.cpp \
+linux:SOURCES   += x11.cpp \
                 x11_iconprovider.cpp \
                 x11_platform.cpp \
-				x11_keytrigger.cpp
-win32:SOURCES	+= win.cpp \
+                x11_keytrigger.cpp
+win32:SOURCES   += win.cpp \
                 win_iconprovider.cpp \
                 win_platform.cpp \
                 win_keytrigger.cpp \
                 win_minidump.cpp \
                 win_util.cpp
-mac:SOURCES		+= mac.cpp \
+macx:SOURCES    += mac.cpp \
                 mac_platform.cpp \
                 mac_keytrigger.cpp
 
