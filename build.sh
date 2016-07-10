2bfdbd10f75e2e77203476dd88b17b144b625398
Jörg Preiß <joerg.preiss@slesa.de>
2016-07-10 12:44:48 +0200

    Start tests with cmake when building. Use cmake in build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-platform='unknown'
-unamestr=`uname`
+set -e
 
-if [[ "$unamestr" == 'Darwin' ]]; then
-   platform='mac'
-elif [[ "$unamestr" == 'Linux' ]]; then
-   platform='lin'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-   platform='bsd'
-fi
-
-echo Creating makefiles ...
-
-pushd .
 cd src
-$QTDIR/bin/qmake -recursive Launchy.pro "CONFIG+=release"
+mkdir -p build && cd build
+cmake ..
+make
+ctest
+
+
+#$platform='unknown'
+#unamestr=`uname`
+#
+#if [[ "$unamestr" == 'Darwin' ]]; then
+#   platform='mac'
+#elif [[ "$unamestr" == 'Linux' ]]; then
+#   platform='lin'
+#elif [[ "$unamestr" == 'FreeBSD' ]]; then
+#   platform='bsd'
+#fi
+
+#echo Creating makefiles ...
+
+#pushd .
+#cd src
+#$QTDIR/bin/qmake -recursive Launchy.pro "CONFIG+=release"
 
 # $QTDIR/bin/lupdate src/Launchy.pro
 
@@ -26,18 +35,18 @@ $QTDIR/bin/qmake -recursive Launchy.pro "CONFIG+=release"
 # $QTDIR/bin/lrelease src/plugins/verby/verby.pro
 # $QTDIR/bin/lrelease src/plugins/weby/weby.pro
 
-echo Building ...
-make
+#echo Building ...
+#make
 
-popd
+#popd
 
-echo Creating setup ...
-if [[ $platform == 'lin' ]]; then
-   sh -x setup/linux/create-.sh
-elif [[ $platform == 'mac' ]]; then
-   sh -x setup/mac/create-setup.sh
-elif [[ $platform == 'bsd' ]]; then
-   sh -x setup/bsd/create-setup.sh
-fi
+#echo Creating setup ...
+#if [[ $platform == 'lin' ]]; then
+#   sh -x setup/linux/create-.sh
+#elif [[ $platform == 'mac' ]]; then
+#   sh -x setup/mac/create-setup.sh
+#elif [[ $platform == 'bsd' ]]; then
+#   sh -x setup/bsd/create-setup.sh
+#fi
 
 echo Done
