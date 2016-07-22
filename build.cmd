f5bf067df72f91788f40c34d5d20517436985ad8
Jörg Preiß <joerg.preiss@slesa.de>
2016-07-22 23:42:36 +0200

    CMake nsis compiler running for windows

--- build.cmd
+++ build.cmd
@@ -5,13 +5,17 @@ call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd6
 cd src
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+REM cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "NMake Makefiles" -config Release ..
 if errorlevel 1 goto ERROR
 
 nmake
 if errorlevel 1 goto ERROR
 
 ctest
+if errorlevel 1 goto ERROR
+
+cpack
 
 :ERROR
 cd ..\..
